eaeac7070fd2ed2c25db31c6443b2f0581163c0a
mika-alpha <yuukeikioku@gmail.com>
2022-08-30 22:34:43 -0500

    Add new todo button to dom

--- src/projectView.js
+++ src/projectView.js
@@ -3,9 +3,13 @@ import { createElementWithClass } from "./dom";
 const projectView = createElementWithClass('div', 'project-view');
 const projectTitle = createElementWithClass('h2', 'project-title');
 const toDosDiv = createElementWithClass('div', 'todos-div');
+const newToDoButton = createElementWithClass('button', 'new-todo-button');
+newToDoButton.setAttribute('type','button');
+newToDoButton.textContent = "+";
 
 projectView.append(projectTitle);
 projectView.append(toDosDiv);
+projectView.append(newToDoButton);
 
 function setTitle(project){
     projectTitle.textContent = project.getTitle();
